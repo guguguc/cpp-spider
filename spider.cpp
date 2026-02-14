54fcdb284136c2712e01fc4da1a2fdec44745e92
gugugu <whatoget@gmail.com>
2026-02-14 06:41:36 +0000

    refactor: add callback system and control flags to Spider
    - Add UserCallback and WeiboCallback function types for GUI integration
    - Add setCrawlWeibo(), setCrawlFans(), setCrawlFollowers() to control crawling
    - Add setUserCallback() and setWeiboCallback() to set callback handlers
    - Add stop() method to halt spider execution
    - Add isRunning() to check spider status
    - Add m_running flag for clean shutdown
    - Improve cookie handling from cookie.json
    - Update rate limiting (100 API calls per 10 seconds instead of 80 per 5)
    - Add logging for debugging API responses

--- spider.cpp
+++ spider.cpp
@@ -16,16 +16,34 @@ using json = nlohmann::json;
 
 Spider::Spider(uint64_t uid) : m_writer("mongodb://0.0.0.0:27017") {
   m_visit_cnt = 0;
+  m_crawlWeibo = true;
+  m_crawlFans = true;
+  m_crawlFollowers = true;
+  m_running = false;
   m_self = User(uid, "", std::vector<User>());
-  m_client = std::make_unique<httplib::Client>("https://weibo.com");
+  m_client = std::make_unique<httplib::Client>("https://www.weibo.com");
   httplib::Headers header;
-  std::ifstream ifs("/home/gugugu/Repo/cpp-spider/build/headers.json");
-  json json_headers = json::parse(ifs);
+
+  std::ifstream cookie_ifs("/home/gugugu/Repo/cpp-spider/cookie.json");
+  json json_cookie = json::parse(cookie_ifs);
+  std::string cookie;
+  for (json::iterator it = json_cookie.begin(); it != json_cookie.end();
+       ++it) {
+    if (!cookie.empty()) {
+      cookie += "; ";
+    }
+    cookie += it.key() + "=" + it.value().get<std::string>();
+  }
+  header.insert(std::make_pair("Cookie", cookie));
+
+  std::ifstream headers_ifs("/home/gugugu/Repo/cpp-spider/headers.json");
+  json json_headers = json::parse(headers_ifs);
   for (json::iterator it = json_headers.begin(); it != json_headers.end();
        ++it) {
     header.insert(std::make_pair<std::string, std::string>(
         std::string(it.key()), it.value().get<std::string>()));
   }
+
   m_client->set_default_headers(header);
   m_client->set_read_timeout(30, 0);
   m_client->set_write_timeout(30, 0);
@@ -34,42 +52,101 @@ Spider::Spider(uint64_t uid) : m_writer("mongodb://0.0.0.0:27017") {
   m_client->set_keep_alive(true);
 }
 
+void Spider::setUserCallback(UserCallback callback) {
+  m_userCallback = std::move(callback);
+}
+
+void Spider::setCrawlWeibo(bool crawl) {
+  m_crawlWeibo = crawl;
+}
+
+void Spider::setWeiboCallback(WeiboCallback callback) {
+  m_weiboCallback = std::move(callback);
+}
+
+void Spider::setCrawlFans(bool crawl) {
+  m_crawlFans = crawl;
+}
+
+void Spider::setCrawlFollowers(bool crawl) {
+  m_crawlFollowers = crawl;
+}
+
+void Spider::stop() {
+  m_running = false;
+}
+
+void Spider::notifyUserFetched(uint64_t uid, const std::string& name,
+                              const std::vector<uint64_t>& followers,
+                              const std::vector<uint64_t>& fans) {
+  if (m_userCallback) {
+    m_userCallback(uid, name, followers, fans);
+  }
+}
+
 User Spider::get_user(uint64_t uid, bool get_follower) {
+  if (!m_running) {
+    return User(uid, "", {});
+  }
   m_visit_cnt++;
-  if (m_visit_cnt % 100 == 0) {
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+  if (m_visit_cnt % 80 == 0) {
+    std::this_thread::sleep_for(std::chrono::seconds(10));
   }
 task:
+  if (!m_running) {
+    return User(uid, "", {});
+  }
   const std::string url = fmt::format("/ajax/profile/info?uid={}", uid);
+  spdlog::info(url);
   try {
     httplib::Result resp = m_client->Get(url);
     auto json_resp = json::parse(resp->body);
-    if (!json_resp.contains("ok")) {
+    spdlog::info(json_resp.dump());
+    if (!json_resp.contains("ok") || json_resp["ok"].get<int>() != 1) {
       return User(uid, "", {});
     }
     auto user = json_resp["data"]["user"];
     auto name = user["screen_name"].get<std::string>();
+    spdlog::info(fmt::format("screen name:{}", name));
     spdlog::info(fmt::format("uid: {}, user name {}", uid, name));
+
+    std::vector<uint64_t> follower_ids;
+    std::vector<uint64_t> fan_ids;
     if (get_follower) {
       User user(uid, name, {});
-      user.followers = get_self_follower();
+      if (m_crawlFollowers) {
+        user.followers = get_self_follower();
+        for (const auto& f : user.followers) {
+          follower_ids.push_back(f.uid);
+        }
+      }
+      if (m_crawlFans) {
+        auto fans = get_other_follower(uid);
+        for (const auto& f : fans) {
+          fan_ids.push_back(f.uid);
+        }
+      }
+      notifyUserFetched(uid, name, follower_ids, fan_ids);
       return user;
     } else {
+      notifyUserFetched(uid, name, {}, {});
       User user(uid, name, {});
       return user;
     }
   } catch (const std::exception &e) {
     spdlog::error(e.what());
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    goto task;
+    if (m_running) {
+      std::this_thread::sleep_for(std::chrono::seconds(10));
+      goto task;
+    }
   }
+  return User(uid, "", {});
 }
 
 std::vector<User> Spider::get_self_follower() {
   std::vector<uint64_t> ids;
   const std::string url =
-      fmt::format("https://weibo.com/ajax/friendships/"
-                  "friends?uid={}&relate=fans&count=20&fansSortType=fansCount",
+      fmt::format("/ajax/friendships/friends?uid={}&relate=fans&count=20&fansSortType=fansCount",
                   m_self.uid);
   auto resp = json::parse(m_client->Get(url)->body);
   std::vector<json::basic_json::object_t> users = resp["users"];
@@ -85,7 +162,7 @@ std::vector<User> Spider::get_other_follower(uint64_t uid) {
   spdlog::info(fmt::format("start to get other follower, uid: {}", uid));
   int page_cnt = 1;
   std::vector<uint64_t> ids;
-  while (1) {
+  while (m_running) {
     const std::string url = fmt::format(
         "/ajax/friendships/"
         "friends?relate=fans&page={}&uid={}&type=all&newFollowerCount=0",
@@ -117,23 +194,37 @@ std::vector<User> Spider::get_other_follower(uint64_t uid) {
 }
 
 void Spider::run() {
+  m_running = true;
   User user = get_user(m_self.uid, true);
   spdlog::info("get user");
-  user.set_weibo(get_weibo(user));
-  m_writer.write_one(user);
-  spdlog::info("write uid: {} to mongodb!", user.uid);
-  for (auto &item : user.followers) {
-    auto followers = get_other_follower(item.uid);
-    item.followers = followers;
-    item.set_weibo(get_weibo(item));
-    m_writer.write_one(item);
-    spdlog::info("write uid: {} to mongodb!", item.uid);
+  if (!m_running) return;
+  if (m_crawlWeibo) {
+    user.set_weibo(get_weibo(user));
+    if (m_weiboCallback) {
+      m_weiboCallback(user.uid, user.weibo);
+    }
   }
+  m_writer.write_one(user);
+  // spdlog::info("write uid: {} to mongodb!", user.uid);
+  // for (auto &item : user.followers) {
+  //   if (!m_running) return;
+  //   auto followers = get_other_follower(item.uid);
+  //   item.followers = followers;
+  //   if (m_crawlWeibo) {
+  //     item.set_weibo(get_weibo(item));
+  //     if (m_weiboCallback) {
+  //       m_weiboCallback(item.uid, item.weibo);
+  //     }
+  //   }
+  //   m_writer.write_one(item);
+  spdlog::info("write uid: {} to mongodb!", user.uid);
+  // }
 }
 
 std::vector<User> Spider::batch_get_user(const std::vector<uint64_t> &ids) {
   std::vector<User> ret;
   for (const auto &id : ids) {
+    if (!m_running) break;
     ret.push_back(get_user(id));
   }
   return ret;
@@ -142,7 +233,7 @@ std::vector<User> Spider::batch_get_user(const std::vector<uint64_t> &ids) {
 std::vector<Weibo> Spider::get_weibo(const User &user) {
   int page_cnt = 1;
   std::vector<Weibo> weibos;
-  while (1) {
+  while (m_running) {
     const std::string url = fmt::format(
         "/ajax/statuses/mymblog?uid={}&page={}&", user.uid, page_cnt);
     auto resp = json::parse(m_client->Get(url)->body);
@@ -166,7 +257,15 @@ std::vector<Weibo> Spider::get_weibo(const User &user) {
           urls.push_back(url);
         }
       }
-      Weibo weibo(text, tm, id, urls);
+      std::string video_url;
+      if (item.count("page_info") && item["page_info"].count("media_info") && 
+          item["page_info"]["media_info"].count("stream_url")) {
+        video_url = item["page_info"]["media_info"]["stream_url"].get<std::string>();
+        if (video_url.find("http") != 0) {
+          video_url = "";
+        }
+      }
+      Weibo weibo(text, tm, id, urls, video_url);
       spdlog::info(weibo.dump());
       weibos.push_back(weibo);
     }
--- spider.hpp
+++ spider.hpp
@@ -3,6 +3,7 @@
 
 
 #include <vector>
+#include <functional>
 #include <httplib.h>
 #include "weibo.hpp"
 #include "writer.hpp"
@@ -10,7 +11,19 @@
 
 class Spider {
 public:
-  Spider(uint64_t user_id);
+  using UserCallback = std::function<void(uint64_t uid, const std::string& name, 
+                                           const std::vector<uint64_t>& followers, 
+                                           const std::vector<uint64_t>& fans)>;
+  using WeiboCallback = std::function<void(uint64_t uid, const std::vector<Weibo>& weibos)>;
+
+  explicit Spider(uint64_t user_id);
+  void setUserCallback(UserCallback callback);
+  void setWeiboCallback(WeiboCallback callback);
+  void setCrawlWeibo(bool crawl);
+  void setCrawlFans(bool crawl);
+  void setCrawlFollowers(bool crawl);
+  void stop();
+  bool isRunning() const { return m_running; }
   User get_user(uint64_t uid, bool get_follower=false);
   std::vector<User> get_self_follower();
   std::vector<User> get_other_follower(uint64_t uid);
@@ -18,11 +31,20 @@ public:
   void run();
 private:
   std::vector<User> batch_get_user(const std::vector<uint64_t> &ids);
+  void notifyUserFetched(uint64_t uid, const std::string& name, 
+                        const std::vector<uint64_t>& followers, 
+                        const std::vector<uint64_t>& fans);
 private:
   User m_self;
   std::unique_ptr<httplib::Client> m_client; 
   uint64_t m_visit_cnt;
   MongoWriter m_writer;
+  UserCallback m_userCallback;
+  WeiboCallback m_weiboCallback;
+  bool m_crawlWeibo;
+  bool m_crawlFans;
+  bool m_crawlFollowers;
+  bool m_running;
 };
 
 #endif  // SPIDER
